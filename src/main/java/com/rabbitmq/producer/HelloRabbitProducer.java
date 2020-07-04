34ab68677b9330dcfc991d98c33e13278efce08c
M Firman Akbar <em.firman.akbar@gmail.com>
2020-07-04 14:43:02 +0700

    adding RabbitMQ producer sample

---
+++ src/main/java/com/rabbitmq/producer/HelloRabbitProducer.java
@@ -0,0 +1,17 @@
+package com.rabbitmq.producer;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloRabbitProducer {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public void sendHello(String name) {
+        rabbitTemplate.convertAndSend("testproducer.hello", "hello " + name);
+    }
+
+}

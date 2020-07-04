f88bbe11a9f9b041bf2e02c7df9ba765a265f279
mfirmanakbar <em.firman.akbar@gmail.com>
2020-07-04 15:36:48 +0700

    producer RabbitMQ with scheduler

---
+++ src/main/java/com/rabbitmq/producer/FixedRateProducer.java
@@ -0,0 +1,27 @@
+package com.rabbitmq.producer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FixedRateProducer {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    private int i = 0;
+
+    private Logger logger = LoggerFactory.getLogger(FixedRateProducer.class);
+
+    @Scheduled(fixedRate = 500)
+    private void sendMessage() {
+        i++;
+        logger.info("i is {}", i);
+        rabbitTemplate.convertAndSend("test.fixedrate", "Fixed Rate " + i);
+    }
+
+}
